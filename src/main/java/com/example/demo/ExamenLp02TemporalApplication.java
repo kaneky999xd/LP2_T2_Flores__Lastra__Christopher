ee7e6191797d88081114f0b771965b81b6349375
sofia rocio lastra valerio <chrislastra90@gmail.com>
2024-06-22 21:59:45 -0500

    iniciando examen

---
+++ src/main/java/com/example/demo/ExamenLp02TemporalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenLp02TemporalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenLp02TemporalApplication.class, args);
+	}
+
+}
